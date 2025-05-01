21ed9ddfaa68390340811110d3d5cee9672e5e5a
boyrazcan33 <ugurboy94@gmail.com>
2025-05-01 05:09:18 +0300

    Service and DTO(fixed)

--- onnoto-backend/src/main/java/com/onnoto/onnoto_backend/dto/request/PreferenceRequest.java
+++ onnoto-backend/src/main/java/com/onnoto/onnoto_backend/dto/request/PreferenceRequest.java
@@ -1,4 +1,10 @@
 package com.onnoto.onnoto_backend.dto.request;
 
+import lombok.Data;
+
+@Data
 public class PreferenceRequest {
+    private String deviceId;
+    private String preferenceKey;
+    private String preferenceValue;
 }
